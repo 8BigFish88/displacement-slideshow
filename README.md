e6880955000ef8c0bdf974c85634912f2ad3a6bf
Ivana Zaccheddu <ivana.z@dbs.agency>
2022-09-16 16:45:04 +0200

    remove everything not related to displacement slideshow, rename class

---
+++ README.md
@@ -0,0 +1,33 @@
+#DISPLACEMENT SLIDESHOW
+
+Javascript library to build slideshow of images with a displacement effect on transition.
+
+This Library is fully based on robin-dela / hover-effect and AlbanCrepel/vue-displacement-slideshow
+
+Is meant to have a way to build a slideshow with a displacement effect.
+
+This allows to do so also with not full screen png pictures, keeping the background transparent.
+
+
+
+#DEMO
+To load the images you'll need to view the demo via a web server.
+
+- go to the root folder location and type python3 -m http.server 8000  in your terminal
+
+- access it in your browser on localhost:8000
+
+
+#USAGE
+- check the basic usage on the file index.html
+
+
+
+
+#Node JS usage
+
+- Install : npm install displacement-slideshow
+- Import : import hoverEffect from 'displacement-slideshow'
+- user like on index.html by creating a new displacementSlideshow
+
+
---
+++ src/displacement-slideshow.js
@@ -0,0 +1,289 @@
+import * as THREE from 'three';
+import TweenMax from 'gsap/TweenMax';
+
+export default function (opts) {
+
+  var vertex = `
+varying vec2 vUv;
+void main() {
+  vUv = uv;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
+`;
+
+  var fragment = `
+varying vec2 vUv;
+
+uniform float dispFactor;
+uniform float dpr;
+uniform sampler2D disp;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform float angle1;
+uniform float angle2;
+uniform float intensity1;
+uniform float intensity2;
+uniform vec4 res;
+uniform vec2 parent;
+
+mat2 getRotM(float angle) {
+  float s = sin(angle);
+  float c = cos(angle);
+  return mat2(c, -s, s, c);
+}
+
+void main() {
+  vec4 disp = texture2D(disp, vUv);
+  vec2 dispVec = vec2(disp.r, disp.g);
+
+  vec2 uv = 0.5 * gl_FragCoord.xy / (res.xy) ;
+  vec2 myUV = (uv - vec2(0.5))*res.zw + vec2(0.5);
+
+
+  vec2 distortedPosition1 = myUV + getRotM(angle1) * dispVec * intensity1 * dispFactor;
+  vec2 distortedPosition2 = myUV + getRotM(angle2) * dispVec * intensity2 * (1.0 - dispFactor);
+  vec4 _texture1 = texture2D(texture1, distortedPosition1);
+  vec4 _texture2 = texture2D(texture2, distortedPosition2);
+  gl_FragColor = mix(_texture1, _texture2, dispFactor);
+}
+`;
+
+  // please respect authorship and do not remove
+  console.log('%c Hover effect by Robin Delaporte: https://github.com/robin-dela/hover-effect ', 'color: #bada55; font-size: 0.8rem');
+
+
+  function firstDefined() {
+    for (var i = 0; i < arguments.length; i++) {
+      if (arguments[i] !== undefined) return arguments[i];
+    }
+  }
+
+  var parent = opts.parent;
+  var dispImage = opts.displacementImage;
+  var image1 = opts.image1;
+  var image2 = opts.image2;
+  var images =  firstDefined(opts.images,[opts.image1,opts.image2]);
+  var imagesRatio = firstDefined(opts.imagesRatio, 1.0);
+  var intensity1 = firstDefined(opts.intensity1, opts.intensity, 1);
+  var intensity2 = firstDefined(opts.intensity2, opts.intensity, 1);
+  var commonAngle = firstDefined(opts.angle, Math.PI / 4); // 45 degrees by default, so grayscale images work correctly
+  var angle1 = firstDefined(opts.angle1, commonAngle);
+  var angle2 = firstDefined(opts.angle2, -commonAngle * 3);
+  var speedIn = firstDefined(opts.speedIn, opts.speed, 1.6);
+  var speedOut = firstDefined(opts.speedOut, opts.speed, 1.2);
+  var easing = firstDefined(opts.easing, Expo.easeOut);
+  var transitionDelay = firstDefined(opts.transitionDelay, 3000);
+
+  var currentImage = 0;
+  var nextImage = 1;
+  var mat = undefined;
+  var object = undefined;
+  var geometry = undefined;
+  var texture1 = null;
+  var texture2 = null;
+  var disp = null;
+  var loader = null;
+  var slideshowId = null;
+
+  if (!parent) {
+    console.warn('Parent missing');
+    return;
+  }
+
+  if (!((images || (image1 && image2)) && dispImage)) {
+    console.warn('One or more images are missing');
+    return;
+  }
+
+  var scene = new THREE.Scene();
+  var camera = new THREE.OrthographicCamera(
+      parent.offsetWidth / -2,
+      parent.offsetWidth / 2,
+      parent.offsetHeight / 2,
+      parent.offsetHeight / -2,
+      1,
+      1000
+  );
+
+  var renderer = new THREE.WebGLRenderer({
+    antialias: false,
+    alpha: true
+  });
+
+  var imageAspect = imagesRatio;
+
+  var render = function () {
+    // This will be called by the TextureLoader as well as TweenMax.
+    renderer.render(scene, camera);
+  };
+
+  function setLoader() {
+    loader = new THREE.TextureLoader();
+    loader.crossOrigin = '';
+  }
+
+  function setDisplacement() {
+    disp = loader.load(dispImage, render);
+    disp.magFilter = disp.minFilter = THREE.LinearFilter;
+  }
+
+  function setMaterial() {
+    mat = new THREE.ShaderMaterial({
+      uniforms: {
+        intensity1: {
+          type: 'f',
+          value: intensity1
+        },
+        intensity2: {
+          type: 'f',
+          value: intensity2
+        },
+        dispFactor: {
+          type: 'f',
+          value: 0.0
+        },
+        angle1: {
+          type: 'f',
+          value: angle1
+        },
+        angle2: {
+          type: 'f',
+          value: angle2
+        },
+        texture1: {
+          type: 't',
+          value: texture1
+        },
+        texture2: {
+          type: 't',
+          value: texture2
+        },
+        disp: {
+          type: 't',
+          value: disp
+        },
+        res: {
+          type: 'vec4',
+          value: new THREE.Vector4(parent.offsetWidth, parent.offsetHeight, a1, a2)
+        },
+        dpr: {
+          type: 'f',
+          value: window.devicePixelRatio
+        }
+      },
+
+      vertexShader: vertex,
+      fragmentShader: fragment,
+      transparent: true,
+      opacity: 1.0,
+    });
+  }
+
+  function setMaterialForNextTransition() {
+    mat.uniforms.texture1.value = texture1;
+    mat.uniforms.texture2.value = texture2;
+    mat.uniforms.dispFactor.value = 0;
+  }
+
+  function setScene() {
+    scene = new THREE.Scene();
+    geometry = new THREE.PlaneBufferGeometry(parent.offsetWidth, parent.offsetHeight, 1);
+    object = new THREE.Mesh(geometry, mat);
+    scene.add(object);
+  }
+
+  function transitionIn() {
+    TweenMax.to(mat.uniforms.dispFactor, speedIn, {
+      value: 1,
+      ease: easing,
+      onUpdate: render,
+      onComplete: render,
+    });
+  }
+
+  function transitionOut() {
+    TweenMax.to(mat.uniforms.dispFactor, speedOut, {
+      value: 0,
+      ease: easing,
+      onUpdate: render,
+      onComplete: render
+    });
+  }
+
+  function playSlideshow() {
+    var index = 0
+    setImage(index)
+    slideshowId = setInterval(() => {
+      transitionIn()
+      index = nextImage
+      setImage(index)
+      loadTexturesImages()
+      setMaterialForNextTransition()
+    }, transitionDelay);
+    return slideshowId;
+  }
+
+  function stopSlideshow() {
+    slideshowId && clearInterval(slideshowId);
+  }
+
+  function setImage(index) {
+    currentImage = index
+    nextImage = (index == (images.length - 1)) ? 0 : (index + 1)
+  }
+
+  function loadTexturesImages() {
+    texture1 = texture2 || loader.load(images[currentImage], render);
+    texture2 = loader.load(images[nextImage], render);
+
+    texture1.magFilter = texture2.magFilter = THREE.LinearFilter;
+    texture1.minFilter = texture2.minFilter = THREE.LinearFilter;
+  }
+
+  camera.position.z = 1;
+
+  renderer.setPixelRatio(2.0);
+  renderer.setClearColor(0xffffff, 0.0);
+  renderer.setSize(parent.offsetWidth, parent.offsetHeight);
+  parent.appendChild(renderer.domElement);
+
+  let a1, a2;
+  if (parent.offsetHeight / parent.offsetWidth < imageAspect) {
+    a1 = 1;
+    a2 = parent.offsetHeight / parent.offsetWidth / imageAspect;
+  } else {
+    a1 = (parent.offsetWidth / parent.offsetHeight) * imageAspect;
+    a2 = 1;
+  }
+
+  setLoader()
+
+  setDisplacement()
+
+  loadTexturesImages();
+
+  setMaterial();
+
+  setScene()
+
+  window.addEventListener('resize', function (e) {
+    if (parent.offsetHeight / parent.offsetWidth < imageAspect) {
+      a1 = 1;
+      a2 = parent.offsetHeight / parent.offsetWidth / imageAspect;
+    } else {
+      a1 = (parent.offsetWidth / parent.offsetHeight) * imageAspect;
+      a2 = 1;
+    }
+    object.material.uniforms.res.value = new THREE.Vector4(parent.offsetWidth, parent.offsetHeight, a1, a2);
+    renderer.setSize(parent.offsetWidth, parent.offsetHeight);
+
+    render()
+  });
+
+  this.next = transitionIn;
+  this.playSlideshow = playSlideshow;
+  this.stopSlideshow = stopSlideshow;
+  this.previous = transitionOut;
+};
+
